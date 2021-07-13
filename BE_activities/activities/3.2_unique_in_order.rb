cea2742b601b4f06a9c151992ea71bd1139d3722
Edwin Sacalamitao <edwin@avionschool.com>
2021-07-13 15:45:47 +0800

    updated code

--- BE_activities/activities/3.2_unique_in_order.rb
+++ BE_activities/activities/3.2_unique_in_order.rb
@@ -1,15 +1,15 @@
 def unique_in_order(arg)
-    arr = []
-    if arg.class != Array
-      arg = arg.squeeze
-      arg = arg.split('')
-      arr.push(arg)
-    elsif arg.class == Array
-      arg.each_with_index {|x,i| arr << x if arg[i-1] != x || i == 0}
-    arr.flatten  
-    end
+  arr = []
+  if arg.class != Array
+    arg = arg.squeeze
+    arg = arg.split('')
+    arr.push(arg)
+  else arg.class == Array
+    arg.each_with_index {|x,i| arr << x if arg[i-1] != x || i == 0}
   end
-  
-  p unique_in_order('AAAABBBCCDAABBB')
-  p unique_in_order('ABBCcAD') 
-  p unique_in_order([1,2,2,3,3])
+  arr.flatten  
+end
+
+p unique_in_order('AAAABBBCCDAABBB')
+p unique_in_order('ABBCcAD') 
+p unique_in_order([1,2,2,3,3])
